ec2e458d64f3cf6b254812c30bfa266472551e61
nikeshDeula <nikeshdeula.21@gmail.com>
2022-10-28 21:58:54 +0545

    Array

---
+++ Array.js
@@ -0,0 +1,27 @@
+//Array in Javascript
+
+
+// Creating an Array
+// var animals = ["Pig", "Hen", "Cow", "Goat", "Fish"];
+// console.log(animals);
+
+
+
+
+//Array Length & Ascessing Array
+var animals = ["Pig", "Hen", "Cow", "Goat", "Fish"];
+var flowers = ["Rose", "Lily", "Sunflower", "Tulip", "Jasmine"];
+
+console.log(animals[3]);
+console.log(animals.length); 
+
+
+
+
+
+
+
+
+
+
+

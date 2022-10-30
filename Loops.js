9653b4681389b928297292af23ae4412206d1db5
nikeshDeula <nikeshdeula.21@gmail.com>
2022-10-30 09:11:39 +0545

    if Else case

--- Loops.js
+++ Loops.js
@@ -3,8 +3,55 @@
 
 // while loop
 
+<<<<<<< Loops
 var i = "10"; 
 var text = "text";
+=======
+// var i = "10"; 
+// var text = "text";
+
+// while(i = text) {
+//     console.log("Hello World");
+// }
+
+
+
+
+
+//For Loops --> Array
+
+// let cars = ["tyota", "nissan", "Range Rober", "mustang" ];
+
+// for (let i =0; i < cars.length; i++) {   
+//     console.log(cars);
+// };
+
+
+
+// if Else with Logical Operators
+
+// const i = 20;
+// const a = 30;
+// const b = 50;
+
+// if(i < a || a < b ) {
+//     console.log("i'm loofy");
+
+// }else{
+//     console.log("i'm not");
+// }
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> local
 
 while(i = text) {
     console.log("Hello World");

cb12594192474ebb033036f38fe3cc65568a6c74
nikeshDeula <nikeshdeula.21@gmail.com>
2022-10-30 08:41:45 +0545

    Array method

--- Loops.js
+++ Loops.js
@@ -1,14 +1,44 @@
-// Loops
+// Loops in Javascript
 
 
 // while loop
 
-var i = "10"; 
-var text = "text";
+// var i = "10"; 
+// var text = "text";
 
-while(i = text) {
-    console.log("Hello World");
-}
+// while(i = text) {
+//     console.log("Hello World");
+// }
+
+
+
+
+//For Loops --> Array
+
+// let cars = ["tyota", "nissan", "Range Rober", "mustang" ];
+
+// for (let i =0; i < cars.length; i++) {   
+//     console.log(cars);
+// };
+
+
+let cars = ["tyota", "nissan", "Range Rober", "mustang" ];
+let bike= ["duke", "honda", "Ns 200"];
+
+bike.push("dukati", "splender");
+bike.pop();
+bike.unshift("")
+
+console.log(bike);
+
+
+
+
+
+
+
+
+
 
 
 
